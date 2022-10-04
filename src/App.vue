ed802e4591c945224f549aeb81a414f80eea0bee
giraud florent <florent giraud>
2022-10-04 14:08:45 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        {{ str }}
+    </div>
+</template>
+
+<script setup> 
+    import { ref } from "vue"
+
+    const str = ref("hello world")
+</script>
---
+++ src/bootloader.js
@@ -0,0 +1,10 @@
+import Vue from "vue"
+
+import "./index.css"
+
+import App from "./App.vue"
+
+new Vue({
+    el: "#app",
+    render: h => h(App)
+})
